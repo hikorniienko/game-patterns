94cfb187a5ace1c59c846353a02f33874796005b
@v.korniienko <v.korniienko@turbogames.io>
2024-03-18 12:40:06 +0200

    Add new files and remove unused files

--- src/design/Command/example.js
+++
@@ -1,1 +0,0 @@
-console.log("Command pattern example: >>>");
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.ts",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devServer: {
+        static: "./dist",
+        port: 8080,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+    ],
+    optimization: {
+        minimize: true,
+    },
+};
