730b2fd9c96a345f62dbd1d0cd64bd594530ee5c
@v.korniienko <v.korniienko@turbogames.io>
2024-03-18 15:13:58 +0200

    Add ESLint configuration and update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = 'asdasd';
-  
-    return element;
-  }
-  document.body.appendChild(component());
+function component (): HTMLElement {
+  const element = document.createElement('div')
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = 'asdasd'
+
+  return element
+}
+
+document.body.appendChild(component())
