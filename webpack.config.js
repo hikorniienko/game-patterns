d51a3be1c7d9dd2bd39396c1bb4f95c026c11371
@v.korniienko <v.korniienko@turbogames.io>
2024-03-18 22:16:13 +0200

    Add .gitignore file and update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: ["./src/index.ts", "./src/design/Command/index.ts"],
+    entry: ["./src/design/Command/index.ts"],
     module: {
         rules: [
             {
